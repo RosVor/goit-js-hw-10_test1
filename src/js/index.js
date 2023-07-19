caccab81e54fd4f3d907985ffd0027c622b6ccdb
RosVor <ros.vorokh@gmail.com>
2023-07-19 15:58:47 +0300

    rewrited

--- src/js/index.js
+++ src/js/index.js
@@ -4,55 +4,51 @@ const breedSelect = document.querySelector(".breed-select");
 const loader = document.querySelector(".loader");
 const error = document.querySelector(".error");
 const catInfo = document.querySelector(".cat-info");
+const catImage = document.querySelector(".cat-image");
+const breedName = document.querySelector(".breed-name");
+const breedDescription = document.querySelector(".breed-description");
+const breedTemperament = document.querySelector(".breed-temperament");
 
 loader.style.display = "none";
 error.style.display = "none";
 catInfo.style.display = "none";
 
-async function populateBreedSelect() {
-  try {
-    const breeds = await fetchBreeds();
-    breeds.forEach(breed => {
-      const option = document.createElement("option");
-      option.value = breed.id;
-      option.textContent = breed.name;
-      breedSelect.appendChild(option);
-    });
-    breedSelect.style.display = "block";
-  } catch {
-    error.style.display = "block";
-  }
+function populateBreedSelect(breeds) {
+  breeds.forEach(breed => {
+    const option = document.createElement("option");
+    option.value = breed.id;
+    option.textContent = breed.name;
+    breedSelect.appendChild(option);
+  });
 }
 
-async function showCatInfo(breedId) {
-  try {
-    loader.style.display = "block";
-    const cat = await fetchCatByBreed(breedId);
-    catInfo.innerHTML = "";
-    const image = document.createElement("img");
-    image.src = cat[0].url;
-    image.alt = "Cat";
-    catInfo.appendChild(image);
-    const breedName = document.createElement("h3");
-    breedName.textContent = cat[0].breeds[0].name;
-    catInfo.appendChild(breedName);
-    const description = document.createElement("p");
-    description.textContent = cat[0].breeds[0].description;
-    catInfo.appendChild(description);
-    const temperament = document.createElement("p");
-    temperament.textContent = `Temperament: ${cat[0].breeds[0].temperament}`;
-    catInfo.appendChild(temperament);
-    loader.style.display = "none";
-    catInfo.style.display = "block";
-  } catch {
-    loader.style.display = "none";
-    error.style.display = "block";
-  }
+function showCatInfo(cat) {
+  catImage.src = cat[0].url;
+  breedName.textContent = `Порода: ${cat[0].breeds[0].name}`;
+  breedDescription.textContent = `Опис: ${cat[0].breeds[0].description}`;
+  breedTemperament.textContent = `Темперамент: ${cat[0].breeds[0].temperament}`;
 }
 
-populateBreedSelect();
+fetchBreeds()
+  .then(breeds => {
+    populateBreedSelect(breeds);
+    breedSelect.style.display = "block"; 
+  })
+  .catch(() => {
+    error.style.display = "block"; 
+  });
 
 breedSelect.addEventListener("change", event => {
   const breedId = event.target.value;
-  showCatInfo(breedId);
+  loader.style.display = "block"; 
+  fetchCatByBreed(breedId)
+    .then(cat => {
+      showCatInfo(cat);
+      loader.style.display = "none"; 
+      catInfo.style.display = "block"; 
+    })
+    .catch(() => {
+      loader.style.display = "none"; 
+      error.style.display = "block"; 
+    });
 });
