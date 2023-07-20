e16a9da634f27b57d46d9eacdfcb8a0578da2a34
RosVor <ros.vorokh@gmail.com>
2023-07-20 16:52:12 +0300

    rename fetch to get in cat-api

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,17 +1,20 @@
 import axios from "axios";
 
 axios.defaults.headers.common["x-api-key"] = "live_LzWstKXEcTZWINkexwtGU8aD5s4nwnEklAPCGBqnLHNNWMU6PFZUFZBRoLpj5nqA";
-export function fetchBreeds() {
-  return axios.get("https://api.thecatapi.com/v1/breeds")
+export function getBreeds() {
+  return axios
+    .get("https://api.thecatapi.com/v1/breeds")
     .then(response => response.data)
     .catch(error => {
       console.error("Error while getting the list of breeds:", error);
       throw error;
     });
 }
+
 export function fetchCatByBreed(breedId) {
   const url = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
-  return axios.get(url)
+  return axios
+    .get(url)
     .then(response => response.data)
     .catch(error => {
       console.error("Error getting information about cat:", error);
