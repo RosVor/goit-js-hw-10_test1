8686cde568c33cfb14a2f9ff1cd5da9e1cd388f9
RosVor <ros.vorokh@gmail.com>
2023-07-21 19:30:38 +0300

    change showing error

--- src/js/index.js
+++ src/js/index.js
@@ -18,39 +18,41 @@ getBreeds()
 select.addEventListener('change', onChangeBreed);
 
 function onChangeBreed(e) {
-  e.preventDefault();
-  loader.style.display = 'block';
-  div.style.display = 'none';
-  let breedId = e.target.value;
-  fetchCatByBreed(breedId)
-    .then(data => {
-      loader.style.display = 'none';
-      div.style.display = 'block';
-      div.innerHTML = data.map(element =>
-        `<div><img src="${element.url}" alt="photo cat" width="500" height="400"/></div>`
-      ).join('');
-      const findCat = data[0].breeds.find(option => option.id === breedId);
-      if (findCat) {
-        const markup = `<div>
-          <h2>${findCat.name}</h2>
-          <p>${findCat.description}</p>
-          <h3>Temperament</h3>
-          <p>${findCat.temperament}</p>
-        </div>`;
-        div.insertAdjacentHTML('beforeend', markup);
-      } else {
+    e.preventDefault();
+    loader.style.display = 'block';
+    div.style.display = 'none';
+    error.style.display = 'none';
+    let breedId = e.target.value;
+    fetchCatByBreed(breedId)
+      .then(data => {
+        loader.style.display = 'none';
+        div.style.display = 'block';
+        div.innerHTML = data.map(element =>
+          `<div><img src="${element.url}" alt="photo cat" width="500" height="400"/></div>`
+        ).join('');
+  
+        const findCat = data[0].breeds.find(option => option.id === breedId);
+        if (findCat) {
+          const markup = `<div>
+            <h2>${findCat.name}</h2>
+            <p>${findCat.description}</p>
+            <h3>Temperament</h3>
+            <p>${findCat.temperament}</p>
+          </div>`;
+          div.insertAdjacentHTML('beforeend', markup);
+        } else {
+          showError();
+        }
+      })
+      .catch(error => {
+        console.error(error);
         showError();
-      }
-    })
-    .catch(error => {
-      console.error(error);
-      showError();
-    })
-    .finally(() => loader.style.display = 'none');
-}
-
-function showError() {
-  loader.style.display = 'none';
-  div.style.display = 'none';
-  error.style.display = 'block';
-}
+      })
+      .finally(() => loader.style.display = 'none');
+  }
+  
+  function showError() {
+    loader.style.display = 'none';
+    div.style.display = 'none';
+    error.style.display = 'block';
+  }
